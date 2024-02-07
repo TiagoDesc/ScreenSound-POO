2b42e564d34f1de4c48f1f1e3732a7a535ef5615
TiagoDesc <nobretiago96@gmail.com>
2024-02-07 15:40:51 -0300

    new

---
+++ Atividade1/Atividade1/Banco.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade1
+{
+    internal class Banco
+    {
+        public int numeroIndicador = 1010;
+        public string titular = "Tiago";
+        public double saldo = 700;
+        public double senha = 1234;
+
+        public void ExibirInformacoes()
+        {
+            Console.WriteLine("\nInformações Gerais: ");
+            Console.WriteLine($"Nome do titular: {titular}");
+            Console.WriteLine($"Saldo da conta: {saldo}");
+            Console.WriteLine($"Senha da conta: {senha}");
+        }
+
+    }
+}
---
+++ Atividade1/Atividade1/Program.cs
@@ -0,0 +1,7 @@
+﻿using Atividade1;
+
+Banco conta = new Banco();
+
+Console.WriteLine($"Titular: {conta.titular}.");
+Console.WriteLine($"Saldo: R$ {conta.saldo}.");
+conta.ExibirInformacoes();
