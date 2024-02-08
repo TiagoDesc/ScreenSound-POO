84272953c033e0eb6d17a4c95be84804a961853f
TiagoDesc <nobretiago96@gmail.com>
2024-02-07 23:06:39 -0300

    new

--- ProgramaPricipal/ProgramaPricipal/obj/Debug/net8.0/ProgramaPricipal.AssemblyInfo.cs
+++ ProgramaPricipal/ProgramaPricipal/obj/Debug/net8.0/ProgramaPricipal.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProgramaPricipal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+308afea22bf8faa29bf4aa1c4039dedf92e310e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+741c7757a80c5e64dcaa715281877c07082a5b59")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProgramaPricipal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProgramaPricipal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
