152646232a748f9fe1752fdda0609d51fc092003
TiagoDesc <nobretiago96@gmail.com>
2024-02-11 01:10:48 -0300

    a mimir

---
+++ Atividade2/Atividade2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Channels;
+
+Titular t = new Titular();
+Conta c = new Conta();
+
+t.Nome = "Tiago";
+t.Cpf = "386.927.462-53"; //Random CPF. Chill.
+t.Endereco = "Rua do Lionel Messi.";
+
+c.Titular = t;
+c.Agencia = 1;
+c.Saldo = 1000000.0;
+c.Limite = 100000.0;
+
+Console.WriteLine("Informações do titular: ");
+Console.WriteLine($"Nome: { c.Titular.Nome}.");
+Console.WriteLine($"CPF: {c.Titular.Cpf}.");
+Console.WriteLine($"Endereço: {c.Titular.Endereco}.");
