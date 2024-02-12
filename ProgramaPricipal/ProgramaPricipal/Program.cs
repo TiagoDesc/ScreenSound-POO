06630c5daa22bca119c242e713ca3925b5b0773d
TiagoDesc <nobretiago96@gmail.com>
2024-02-12 01:06:37 -0300

    a mimir, jogar no vasco

--- ProgramaPricipal/ProgramaPricipal/Program.cs
+++ ProgramaPricipal/ProgramaPricipal/Program.cs
@@ -1,18 +1,21 @@
-﻿Album albumDoJamesArthur = new Album();
+﻿Banda jamesArthur = new Banda();
+jamesArthur.Nome = "James Arthur";
+
+Album albumDoJamesArthur = new Album();
 albumDoJamesArthur.Nome = "You";
 
-Musica musica1 = new Musica();
+Musica musica1 = new Musica(jamesArthur);
 musica1.Nome = "Fall";
 musica1.Duracao = 222;
 
-Musica musica2 = new Musica();
+Musica musica2 = new Musica(jamesArthur);
 musica2.Nome = "Car's Outside";
 musica2.Duracao = 248;
 
 albumDoJamesArthur.AdicionarMusica(musica1);
 albumDoJamesArthur.AdicionarMusica(musica2);
 
-Banda jamesArthur = new Banda();
-jamesArthur.Nome = "James Arthur";
+
 jamesArthur.AdicionarAlbum(albumDoJamesArthur);
 jamesArthur.ExibirDiscografia();
+albumDoJamesArthur.ExibirMusicasDoAlbum();
